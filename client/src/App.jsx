95c5127a6d990cb924674aedebe8df9b1553971f
Adrian Hidalgo <hidalgobeltreadrian@gmail.com>
2024-01-05 11:54:48 -0400

    Adding Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import "../public/assets/Styles/index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { Welcome } from "./Pages/Welcome";
 
 function App() {
   return (
     <>
-      <div>
-        
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/welcome" element={<Welcome />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
