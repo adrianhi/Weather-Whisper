306c954a5f28e6991b7318b0399fb88769225e8d
Adrian Hidalgo <hidalgobeltreadrian@gmail.com>
2024-01-25 23:14:05 -0400

    Backend with Login,register and LogOut

---
+++ server/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      trim: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", userSchema);
