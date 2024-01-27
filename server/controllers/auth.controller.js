996cd507e97ddd59e0c3f377ea046872b7c1ad3f
Adrian Hidalgo <hidalgobeltreadrian@gmail.com>
2024-01-26 20:30:41 -0400

    Login,Register and LogOut Server Success

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -13,7 +13,7 @@ export const register = async (req, res) => {
     const userSaved = await newUser.save();
 
     const token = await createAccessToken({ id: userSaved._id });
-    res.cookie("cookie", token);
+    res.cookie("token", token);
 
     res.json({
       id: userSaved._id,
@@ -36,7 +36,7 @@ export const login = async (req, res) => {
     if (!isMatch)
       return res.status(400), json({ message: "Incorrect email or password" });
     const token = await createAccessToken({ id: userFound._id });
-    res.cookie("cookie", token);
+    res.cookie("token", token);
 
     res.json({
       id: userFound._id,
@@ -56,4 +56,16 @@ export const logOut = (req, res) => {
   });
 
   return res.sendStatus(200);
+};
+
+export const profile = async (req, res) => {
+  const userFound = await User.findById(req.user.id);
+  if (!userFound) return res.status(400).json({ message: "User not Found" });
+  res.json({
+    id: userFound._id,
+    username: userFound.username,
+    email: userFound.email,
+    createdAt: userFound.createdAt,
+    updatedAt: userFound.updatedAt,
+  });
 };
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import { register, login, logOut } from "../controllers/auth.controller.js";
-
+import {
+  register,
+  login,
+  logOut,
+  profile,
+} from "../controllers/auth.controller.js";
+import { authRequired } from "../middleware/auth.middleware.js";
+import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
+import { validateSchema } from "../middleware/validator.middleware.js";
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validateSchema(registerSchema), register);
+router.post("/login", validateSchema(loginSchema), login);
 router.post("/logOut", logOut);
+router.get("/profile", authRequired, profile);
 
 export default router;
